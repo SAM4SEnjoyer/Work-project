48dde53f6712d93bc9ccae2424d7a2e8fb57d99c
SAM4SEnjoyer <105928640+SAM4SEnjoyer@users.noreply.github.com>
2022-05-23 10:16:16 +0200

    Create spi_functions.h

---
+++ spi_functions.h
@@ -0,0 +1,26 @@
+
+#ifndef SPI_FUNCTIONS_H
+#define SPI_FUNCTIONS_H
+
+//------------------------------------------------------------------------------
+//         includes
+//------------------------------------------------------------------------------
+
+#include <asf.h>
+
+
+
+//------------------------------------------------------------------------------
+//			Global functions
+//------------------------------------------------------------------------------
+
+/*
+ * Name:		init_SPI
+ * Description:	Fonctions pour initialiser les interfaces SPI
+ * Arguments:
+ */
+void init_SPI(void);
+void Init_SPI_2(void);
+
+
+#endif /* SPI_FUNCTIONS_H */
