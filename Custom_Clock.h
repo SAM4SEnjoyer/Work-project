f1a36d87af68ea6f2d9ec346a75752c4363c1f94
SAM4SEnjoyer <105928640+SAM4SEnjoyer@users.noreply.github.com>
2022-05-30 13:34:23 +0200

    Create Custom_Clock.h

---
+++ Custom_Clock.h
@@ -0,0 +1,18 @@
+/*
+ * Custom_Clock.h
+ *
+ * Created: 25/05/2022 10:00:39
+ *  Author: Etudiant
+ */ 
+
+
+#ifndef CUSTOM_CLOCK_H_
+#define CUSTOM_CLOCK_H_
+
+#include <asf.h>
+void Oscillator_RC(void);
+void Oscillator_Crystal(void);
+void Oscillator_PLLA(void);
+void Oscillator_PLLB(void);
+
+#endif /* CUSTOM_CLOCK_H_ */
